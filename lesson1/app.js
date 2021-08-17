c0844271dd1f74534ba6df910fd11d87d5b16a36
Pavlo1307 <pavloshavel@gmail.com>
2021-08-17 19:28:46 +0300

    something do

--- lesson1/app.js
+++ lesson1/app.js
@@ -13,7 +13,38 @@ const users = [
     {name: "Seryu", gender: "male", age: 14},
     {name: "Vitalik", gender: "male", age: 15}
 ]
-console.log(users)
+
+const pathManOlder20 = path.join(__dirname, 'manOlder20');
+const pathManYounger20 = path.join(__dirname, 'manYounger20');
+const pathWomanOlder20 = path.join(__dirname, 'womanOlder20');
+const pathWomanYounger20 = path.join(__dirname, 'womanYounger20');
+
+fs.mkdir(pathManOlder20, err => {
+    if (err) {
+        console.log(err)
+        return;
+    }
+    fs.mkdir(pathManYounger20, err1 => {
+        if (err1) {
+            console.log(err1)
+            return;
+        }
+        fs.mkdir(pathWomanOlder20, err2 => {
+            if (err2) {
+                console.log(err2)
+                return;
+            }
+            fs.mkdir(pathWomanYounger20, err3 => {
+                if (err3) {
+                    console.log(err3)
+                    return;
+                }
+                console.log('sdsds')
+
+            })
+        })
+    })
+})
 
 
 
