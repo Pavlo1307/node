cfb689c99b8e1019966b53f76e24ca61b288f9ce
Pavlo1307 <pavloshavel@gmail.com>
2021-08-19 19:08:01 +0300

    something do

--- homeTask2New/app.js
+++ homeTask2New/app.js
@@ -8,49 +8,69 @@ const users = require('./db/users')
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs');
 app.engine('.hbs', expressHbs({defaultLayout: false}));
-app.set('views',path.join(__dirname, 'static'));
+app.set('views', path.join(__dirname, 'static'));
 
 app.get('/', ((req, res) => {
     console.log(req);
-
-    //res.end("fsdfsdf")
-    //res.json({name:'viktor'})
     res.status(404).end('not found');
 
 }))
 
 app.get('/login', (req, res) => {
-    res.render('login' );
+    res.render('login');
 })
 
-app.get('/users', (req, res) => {
-    res.render('users', { users });
-})
-
-app.get('/register', (req, res) => {
-    res.render('register' );
-})
 app.post('/login', (req, res) => {
 
-    const { name, password } = req.body;
+    const {name, password} = req.body;
 
     for (const user of users) {
-        if(name === user.name){
-            console.log(name);
-            console.log(user.name)
-            res.status(200).redirect('users');
+        if (name === user.name) {
+            return res.status(200).redirect('/users');
         }
-            res.render('register');
     }
-
+    res.redirect('/register');
 
 });
 
-app.listen(PORT, ()=>{
+app.get('/user/:user_id',(req, res) => {
+    const {user_id} = req.params;
+    const  currentUsers = users[user_id];
+
+    if (!currentUsers){
+        res.status(404).end('User not found!');
+        return;
+    }
+    res.render('userInfo', {currentUsers})
+})
+
+app.get('/users', (req, res) => {
+    res.render('users', {users});
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', (req, res) => {
+    const newUser = req.body;
+
+    for (const user of users) {
+        if( user.name === newUser.name){
+            return res.status(200).end('Login is same',);
+        }
+    }
+    users.push(newUser);
+    console.log(users);
+    res.redirect('/users');
+
+})
+
+app.listen(PORT, () => {
     console.log('App listen', PORT);
 })
