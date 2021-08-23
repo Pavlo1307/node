cbcb5a04002efb9c3f018b2f354271c69c94205e
Pavlo1307 <pavloshavel@gmail.com>
2021-08-23 12:39:59 +0300

    something do

--- homeTask3/routes/login.router.js
+++ homeTask3/routes/login.router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { loginController } = require('../controlles');
 
 router.get('/', loginController.getLogin);
+
 router.post('/', loginController.postLogin);
 
 module.exports = router;
--- homeTask3/routes/user.router.js
+++ homeTask3/routes/user.router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { userController } = require('../controlles');
 
 router.get('/:user_id', userController.getSingleUser);
+
 router.get('/', userController.getAllUsers);
 
 module.exports = router;
